e6a5cdd09632d5b473c97250fa884643839f742b
hilmibakhari <hilmibakhari@yahoo.com>
2019-02-10 00:50:02 +0800

    project base

---
+++ src/main/java/com/hb/udemyrestfulservice/UdemyRestfulServiceApplication.java
@@ -0,0 +1,14 @@
+package com.hb.udemyrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyRestfulServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/hb/udemyrestfulservice/UdemyRestfulServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hb.udemyrestfulservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemyRestfulServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
